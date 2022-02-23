feb9d391a8a21b96ef2f4dccfc3c8b800e227f35
Егор Гривко <kombad56@list.ru>
2022-02-23 18:06:14 +0500

    Добавьте файлы проекта.

---
+++ pz_2s_3/Compony.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_3
+{
+    class Compony
+    {
+        public string name;
+        public int okpo;
+        public DateTime dateOFRegister;
+        public string DateRam;
+        public string OKPO;
+        public string namenull;
+
+        public Compony(string name, int okpo, DateTime data)
+        {
+            this.name = name;
+            if (String.IsNullOrEmpty(name))
+                Console.WriteLine("Название компании не блыо указано");
+            this.okpo = okpo;
+            if (okpo < 9999999)
+                OKPO = @"ОКП Введено неверно";
+            else if (okpo >= 100000000)
+                OKPO =  @"ОКП Введено неверно";
+            Console.WriteLine(OKPO);
+            this.dateOFRegister = data;
+
+
+
+            if (data >= new DateTime(1990, 1, 1) && data <= new DateTime(2022, 1, 1))
+                dateOFRegister = data;
+            else DateRam = @"Временые рамки не верны";
+        }
+
+        public void GetCompanyInfoo()
+        {
+            Console.WriteLine($"Организация {name}, под номером OKPO {okpo} была создана {dateOFRegister}");
+            Console.WriteLine(DateRam);
+            
+        }
+        
+
+    }
+}
---
+++ pz_2s_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pz_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data = new DateTime(2002,2,2);
+            Console.WriteLine("Введите название вашей организации");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите ОКПО вашей организации");
+            
+            int okpo = Convert.ToInt32(Console.ReadLine());
+            Compony new1 = new Compony(name, okpo, data);
+            new1.GetCompanyInfoo();
+        }
+    }
+}
